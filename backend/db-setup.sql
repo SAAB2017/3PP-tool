e727db5fccad4e5d7b8fbb2503065dff3575107a
dat15dmi <33149338+dat15dmi@users.noreply.github.com>
2017-11-02 11:13:54 +0100

    Update db-setup.sql

--- backend/db-setup.sql
+++ backend/db-setup.sql
@@ -1,27 +1,103 @@
 /* Drop tables */
-DROP TABLE IF EXISTS components;
-DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS projectLog;
 DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS productLog;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS componentLog;
+DROP TABLE IF EXISTS components;
+DROP TABLE IF EXISTS licenseLog;
+DROP TABLE IF EXISTS licenses;
+DROP TABLE IF EXISTS Accounts;
 
 /* Create tables */
+CREATE TABLE "Accounts"
+	(accountID TEXT PRIMARY KEY UNIQUE, password TEXT NOT NULL, name TEXT, email TEXT);
+CREATE TABLE "licenses"
+    (licenseName TEXT, licenseVersion TEXT, dateCreated DATE, lastEdited DATETIME, URL TEXT,
+    comment TEXT, licenseType TEXT,
+    PRIMARY KEY (licenseName, licenseVersion)
+    );
 CREATE TABLE "components"
-	(id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, component TEXT NOT NULL, version TEXT NOT NULL);
+    (componentName TEXT, componentVersion TEXT, licenseName TEXT UNIQUE, licenseVersion TEXT UNIQUE,
+    dateCreated DATE, lastEdited DATETIME, comment TEXT, approved BIT, approvedBy TEXT,
+    PRIMARY KEY (componentName, componentVersion),
+    FOREIGN KEY (licenseName, licenseVersion) REFERENCES licenses(licenseName, licenseVersion) ON DELETE CASCADE
+    );
 CREATE TABLE "products"
-	(id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, product TEXT NOT NULL, version TEXT NOT NULL);
+    (productName TEXT, productVersion TEXT, componentName TEXT UNIQUE, componentVersion TEXT UNIQUE,
+    dateCreated DATE, lastEdited DATETIME, comment TEXT, approved BIT, approvedBy TEXT,
+    PRIMARY KEY (productName, productVersion),
+    FOREIGN KEY (componentName, componentVersion) REFERENCES components(componentName, componentVersion) ON DELETE CASCADE
+    );
 CREATE TABLE "projects"
-	(id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, project TEXT NOT NULL, version TEXT NOT NULL);
+    (projectName TEXT, projectVersion TEXT, productName TEXT UNIQUE, productVersion TEXT UNIQUE,
+    dateCreated DATE, lastEdited DATETIME, comment TEXT, approved BIT, approvedBy TEXT,
+    PRIMARY KEY (projectName, projectVersion),
+    FOREIGN KEY (productName, productVersion) REFERENCES products(productName, productVersion) ON DELETE CASCADE
+    );
+CREATE TABLE "licenseLog"
+    (licenseName TEXT, licenseVersion TEXT, dateLogged DATE, note TEXT NOT NULL,
+    PRIMARY KEY (licenseName, licenseVersion),
+    FOREIGN KEY (licenseName, licenseVersion) REFERENCES licenses(licenseName, licenseVersion) ON DELETE CASCADE
+    );
+CREATE TABLE "componentLog"
+    (componentName TEXT, componentVersion TEXT, dateLogged DATE, note TEXT NOT NULL,
+    PRIMARY KEY (componentName, componentVersion),
+    FOREIGN KEY (componentName, componentVersion) REFERENCES components(componentName, componentVersion) ON DELETE CASCADE
+    );
+CREATE TABLE "productLog"
+    (productName TEXT, productVersion TEXT, dateLogged DATE, note TEXT NOT NULL,
+    PRIMARY KEY (productName, productVersion),
+    FOREIGN KEY (productName, productVersion) REFERENCES products(productName, productVersion) ON DELETE CASCADE
+    );
+CREATE TABLE "projectLog"
+    (projectName TEXT, projectVersion TEXT, dateLogged DATE, note TEXT NOT NULL,
+    PRIMARY KEY (projectName, projectVersion),
+    FOREIGN KEY (projectName, projectVersion) REFERENCES projects(projectName, projectVersion) ON DELETE CASCADE
+    );
+
+/* Accounts */
+INSERT INTO Accounts (accountID, password, name, email) VALUES
+	("admin", "admin", "admin", "admin@admin.com"),
+	("markus", "markus", "markus", "nat13msk@student.lu.se");
+
+/* Licenses */
+INSERT INTO licenses (licenseName, licenseVersion, dateCreated, lastEdited, URL, comment, licenseType) VALUES
+	("Test License", "1.01", 0, 0, "www.detfungerar.com", "hello world!", "License type"),
+    ("Test License1", "1.1", 0, 0, "www.detfungerar.com", "hello world!", "License type1"),
+    ("Test License1", "1.2", 0, 0, "www.detfungerar.com", "hello world!", "License type1"),
+    ("Test License2", "1.1", 0, 0, "www.detfungerar.com", "hello world!", "License type");
 
 /* Components */
-INSERT INTO components (component, version) VALUES
-    ("AJAX Control Toolkit", "16.1.0.0"),
-    ("Spectrum Colorpicker", "1.8.0"),
-	("Leaflet", "0.5");
+INSERT INTO components
+    (componentName, componentVersion, licenseName, licenseVersion, dateCreated, lastEdited, comment,
+    approved, approvedBy) VALUES
+    ("Test Components", "1.0", "Test License", "1.01", 0, 0, "component comment", 1, "component approved by");
 
 /* Products */
-INSERT INTO products (product, version) VALUES
-    ("AKKA", "6.0"),
-	("WISE", "6.0");
+INSERT INTO products
+    (productName, productVersion, componentName, componentVersion,
+    dateCreated, lastEdited, comment, approved, approvedBy) VALUES
+    ("Test Products", "1.0", "Test Components", "1.0", 0, 0, "product comment", 1, "product approved by");
 
-/* Projets */
-INSERT INTO projects (project, version) VALUES
-    ("ATSsim", "1.0");
+/* Projects */
+INSERT INTO projects
+    (projectName, projectVersion, productName, productVersion,
+    dateCreated, lastEdited, comment, approved, approvedBy) VALUES
+    ("Test Projects", "1.0", "Test Products", "1.0", 0, 0, "project comment", 1, "project approved by");
+
+/* License Log */
+INSERT INTO licenseLog (licenseName, licenseVersion, dateLogged, note) VALUES
+    ("Test License", "1.01", 0, "License created");
+
+/* Component Log */
+INSERT INTO componentLog (componentName, componentVersion, dateLogged, note) VALUES
+    ("Test Components", "1.0", 0, "Component created");
+
+/* Product Log */
+INSERT INTO productLog (productName, productVersion, dateLogged, note) VALUES
+    ("Test Products", "1.0", 0, "Product created");
+
+/* Project Log */
+INSERT INTO projectLog (projectName, projectVersion, dateLogged, note) VALUES
+    ("Test Projects", "1.0", 0, "Project created");
