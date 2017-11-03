de4a1f6f3f7785b67fb3534dcb1c242a9622099d
Markus.S <nat13msk@student.lu.se>
2017-11-03 14:52:58 +0100

    Add license
    Some changes to the license API.

--- backend/routes/licenses.js
+++ backend/routes/licenses.js
@@ -13,82 +13,9 @@ router.route('/')
         })
     })
 
-    // A parameter 'get' needs to be provided as to indicate wheter or not a specific set of rows need to be gathered otherwise
-    // it is assumed that a row is to be changed
-    //-------------------------------------------------------------------------
-    // Below are the filters that need to be provided if a specific set of data rows are to be returned:
-    // licensName
-    // licenseVersion
-    // dateCreated
-    // lastEdited
-    // licensType
-    //-------------------------------------------------------------------------
-    // TODO - editing a license
-    //-------------------------------------------------------------------------
     .post((req, res) => {
-        if (req.body.get) {
-            var query = "SELECT * FROM licenses WHERE "
-            var parameters = []
-
-            var licensName = req.body.licensName
-            var licenseVersion = req.body.licenseVersion
-            var dateCreated = req.body.dateCreated
-            var lastEdited = req.body.lastEdited
-            var licensType = req.body.licensType
-
-            var first = false
-            if (licensName != null) {
-                if (!first) {
-                    first = true
-                    query += "licenseName = ? "
-                } else {
-                    query += "AND licenseName = ?"
-                }
-                parameters.push(licensName)
-            }
-            if (licenseVersion != null) {
-                if (!first) {
-                    first = true
-                    query += "licenseVersion = ? "
-                } else {
-                    query += "AND licenseVersion = ? "
-                }
-                parameters.push(licenseVersion)
-            }
-            if (dateCreated != null) {
-                if (!first) {
-                    first = true
-                    query += "dateCreated = ? "
-                } else {
-                    query += "AND dateCreated = ? "
-                }
-                parameters.push(dateCreated)
-            }
-            if (lastEdited != null) {
-                if (!first) {
-                    first = true
-                    query += "lastEdited = ? "
-                } else {
-                    query += "AND lastEdited = ? "
-                }
-                parameters.push(lastEdited)
-            }
-            if (licensType != null) {
-                if (!first) {
-                    first = true
-                    query += "licenseType = ? "
-                } else {
-                    query += "AND licenseType = ? "
-                }
-                parameters.push(licensType)
-            }
-
-            req.db.all(query, parameters, (err, rows) => {
-                res.json(rows)
-            })
-        } else {
-            // TODO - implement the functionality for editing a license
-        }
+        res.status(405)
+        res.send("Method not allowed")
     })
 
     // Add a license.
@@ -120,10 +47,10 @@ router.route('/')
         })
     })
 
-    // TODO - implement
+    
     .delete((req, res) => {
-        res.status(501)
-        res.send("Method not implemented")
+        res.status(405)
+        res.send("Method not allowed")
     })
 
 // ----------------------------------------------------------------------------
@@ -168,7 +95,7 @@ router.route('/:id')
         req.db.all(query, parameters, (err, rows) => {
             if (err) {
                 //If there's an error then provide the different attributes that could have caused it. 
-                res.send("Input: " + inputString + ", query: " + query + ", values: " + values + ", valuesText: " + valuesText)
+                res.send("ERROR! Input: " + inputString + ", query: " + query + ", values: " + values + ", valuesText: " + valuesText)
             } else
                 res.json(rows)
         })
@@ -180,17 +107,13 @@ router.route('/:id')
     })
 
     .put((req, res) => {
-
+        res.status(405)
+        res.send("Method not allowed")
     })
 
     .delete((req, res) => {
-        var query = "DELETE FROM components WHERE id = ?"
-        var id = req.params.id
-
-        req.db.get(query, [id], (err, row) => {
-            res.status(200)
-            res.send("Component deleted")
-        })
+        res.status(405)
+        res.send("Method not allowed")
     })
 
 module.exports = router
