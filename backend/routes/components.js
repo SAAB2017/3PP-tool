e0ea68a9db74e0d568fd4f6de9268cf5518a771a
Markus.S <nat13msk@student.lu.se>
2017-11-06 21:52:33 +0100

    API: alter a component
    Implemented functionality and fixed a issues with the component API.

--- backend/routes/components.js
+++ backend/routes/components.js
@@ -66,16 +66,16 @@ router.route('/:id')
 
     // Check if smartSearch.
     var smartSearch = false
-    for(var i = 0; i < inputString.length; i++) {
-      if(inputString[i] == 'smartSearch') smartSearch = true
+    for (var i = 0; i < inputString.length; i++) {
+      if (inputString[i] == 'smartSearch') smartSearch = true
     }
 
     // Get search parameters from input.
     for (var i = 0; i < inputString.length; i++) {
-      if(inputString[i] != 'smartSearch'){
+      if (inputString[i] != 'smartSearch') {
         var tempHolder = inputString[i].split("=")
         valuesText.push(tempHolder[0])
-        if(smartSearch) values.push('%' + tempHolder[1] + '%')
+        if (smartSearch) values.push('%' + tempHolder[1] + '%')
         else values.push(tempHolder[1])
       }
     }
@@ -86,10 +86,10 @@ router.route('/:id')
       if (values[i] != null) {
         if (!first) {
           first = true
-          if(smartSearch) query += "" + valuesText[i] + " LIKE ? "
+          if (smartSearch) query += "" + valuesText[i] + " LIKE ? "
           else query += "" + valuesText[i] + " = ? "
         } else {
-          if(smartSearch) query += "AND " + valuesText[i] + " LIKE ? "
+          if (smartSearch) query += "AND " + valuesText[i] + " LIKE ? "
           else query += "AND " + valuesText[i] + " = ? "
         }
         parameters.push(values[i])
@@ -105,9 +105,94 @@ router.route('/:id')
     })
   })
 
+  // In order to alter a specific component or a specific group of components based on
+  // certain parameters then this methis is to be used; this method must be called with an identifier constructed as follows:
+  // Example input identifier: "componentName=Test Components&licenseVersion=1.0&comment=Here's a new comment!"
+  // Between each provided parameter there must be an '&' character. Aside from that
+  // the order of provided parameters makes no difference.
   .post((req, res) => {
-    res.status(405)
-    res.send("Method not allowed")
+    var values = []
+    var valuesText = []
+
+    var inputString = req.params.id.split("&")
+    //Get input parameters from the input.
+    for (var i = 0; i < inputString.length; i++) {
+      var tempHolder = inputString[i].split("=")
+      valuesText.push(tempHolder[0])
+      values.push(tempHolder[1])
+    }
+
+    var correctInputComponentName = null
+    var correctInputComponentVersion = null
+    var date = [null, false]
+    //Make sure that there is a componentName and componentVersion provided. Also checks if dateCreated and lastEdited is provided.
+    for (var i = 0; i < valuesText.length; i++) {
+      if (valuesText[i] == 'componentName') {
+        correctInputComponentName = values[i]
+      } else if (valuesText[i] == 'componentVersion') {
+        correctInputComponentVersion = values[i]
+      } else if (valuesText[i] == 'dateCreated') {
+        date[0] = i
+        values[date[0]] = new Date().toDateString()
+      } else if (valuesText[i] == 'lastEdited') {
+        values[i] = new Date().toDateString()
+        date[1] = true
+      }
+    }
+
+    //If dateCreated was provided then remove it.
+    if (date[0] != null) {
+      var tempValuesArray = []
+      var tempValuesTextArray = []
+      for (var i = 0; i < valuesText.length; i++) {
+        if (i != date[0]) {
+          tempValuesTextArray.push(valuesText[i])
+          tempValuesArray.push(values[i])
+        }
+      }
+      value = tempValuesArray
+      valuesText = tempValuesTextArray
+    }
+
+    //If lastEdited wasn't provided then provide it.
+    if (!date[1]) {
+      valuesText.push('lastEdited')
+      if (values.length < valuesText.length) values.push(new Date().toDateString())
+    }
+
+    //Make sure the necessary data is provided
+    if (correctInputComponentName != null && correctInputComponentVersion != null) {
+      
+      var query = "UPDATE components SET "
+      var parameters = []
+      //Construct remaining SQL query based on input parameters.
+      var first = false;
+      for (var i = 0; i < valuesText.length; i++) {
+        if (values[i] != null && valuesText[i] != 'licenseID') {
+          if (!first) {
+            first = true
+            query += "" + valuesText[i] + " = ?"
+          } else {
+            query += ", " + valuesText[i] + " = ?"
+          }
+          parameters.push(values[i])
+        }
+      }
+      query += " WHERE componentName = ? AND componentVersion = ?;"
+      parameters.push(correctInputComponentName)
+      parameters.push(correctInputComponentVersion)
+
+      req.db.run(query, parameters, (error) => {
+        if (error) {
+          console.log("1: " + error.message)
+          res.status(500)
+          res.send("ERROR! error message:" + error.message + " Input: " + inputString + ", query: " + query + ", values: " + values + ", valuesText: " + valuesText)
+        } else {
+          res.status(200)
+          res.send("Success!")
+        }
+      })
+    }
   })
 
   //In order to add a new component then
@@ -122,7 +207,7 @@ router.route('/:id')
     var valuesText = []
 
     var inputString = req.params.id.split("&")
-    //Get input parameters from the input
+    //Get input parameters from the input 
     for (var i = 0; i < inputString.length; i++) {
       var tempHolder = inputString[i].split("=")
       valuesText.push(tempHolder[0])
@@ -132,7 +217,8 @@ router.route('/:id')
     var correctInputComponentName = null
     var correctInputComponentVersion = null
     var correctInputLicenseID = null
-    //Make sure that there is a componentName, componentVersion and licenseID provided
+    var date = [false, false]
+    //Make sure that there is a componentName, componentVersion and licenseID provided. Also checks if dateCreated and lastEdited is provided.
     for (var i = 0; i < valuesText.length; i++) {
       if (valuesText[i] == 'componentName') {
         correctInputComponentName = values[i]
@@ -140,9 +226,26 @@ router.route('/:id')
         correctInputComponentVersion = values[i]
       } else if (valuesText[i] == 'licenseID') {
         correctInputLicenseID = values[i]
+      } else if (valuesText[i] == 'dateCreated') {
+        values[i] = new Date().toDateString()
+        date[0] = true
+      } else if (valuesText[i] == 'lastEdited') {
+        values[i] = new Date().toDateString()
+        date[1] = true
       }
     }
+    //If dateCreated wasn't provided then provide it
+    if (!date[0]) {
+      valuesText.push('dateCreated')
+      values.push(new Date().toDateString())
+    }
+    //If lastEdited wasn't provided then provide it
+    if (!date[1]) {
+      valuesText.push('lastEdited')
+      values.push(new Date().toDateString())
+    }
 
+    //Make sure the necessary parameters are provided.
     if (correctInputComponentName != null && correctInputComponentVersion != null && correctInputLicenseID != null) {
 
       //Check if the component already exists. If it exists already then store it's values so they can be inserted along licenseID into a new row.
@@ -151,16 +254,16 @@ router.route('/:id')
       var parametersExists = [correctInputComponentName, correctInputComponentVersion]
       req.db.get(queryExists, parametersExists, (error, row) => {
         if (error) {
-          //If there's an error then provide the error message and the different attributes that could have caused it.
+          //If there's an error then provide the error message and the different attributes that could have caused it. 
           res.send("ERROR! error message:" + error.message + " Input: " + inputString + ", query: " + queryGetID + ", values: " + values + ", valuesText: " + valuesText)
         } else {
           currentRow = row
 
-          if (currentRow != null) {
-            values = [currentRow.componentName, currentRow.componentVersion, correctInputLicenseID, currentRow.dateCreated, currentRow.lastEdited, currentRow.comment, currentRow.approved, currentRow.approvedBy]
+          if (currentRow != null) { //Component exists. Relevant data need to be inserted along with the licenseID.
+            values = [currentRow.componentName, currentRow.componentVersion, correctInputLicenseID, new Date().toDateString(), new Date().toDateString(), currentRow.comment, currentRow.approved, currentRow.approvedBy]
             valuesText = ['componentName', 'componentVersion', 'licenseID', 'dateCreated', 'lastEdited', 'comment', 'approved', 'approvedBy']
 
-            var queryInsert = "INSERT INTO components ( "
+            var queryInsert = "INSERT OR IGNORE INTO components ( "
             //Construct remaining SQL query based on input parametersInsert
             for (var j = 0; j < 2; j++) {
               for (var i = 0; i < valuesText.length; i++) {
@@ -183,7 +286,6 @@ router.route('/:id')
               } else queryInsert += ");"
             }
 
-            //console.log("1currentRow: " + currentRow + ", query: " + queryInsert + ", values: " + parametersInsert)
             req.db.run(queryInsert, parametersInsert, (error) => {
               if (error) {
                 console.log("1: " + error.message)
@@ -192,33 +294,18 @@ router.route('/:id')
               }
             })
 
-            //Log the creation of the components
-            var parametersLog = [currentRow.id, new Date().toDateString(), "Component created."]
-            var queryLog = "INSERT INTO componentLog (componentID, dateLogged, note) VALUES (?, ?, ?);"
-            //console.log("currentRow: " + currentRow + ", query: " + queryLog + "" + queryLog + ", values: " + parametersLog)
-            req.db.run((queryLog), parametersLog, (error) => {
-              if (error) {
-                console.log(error.message)
-                res.status(500)
-                res.send(error.message)
-              } else {
-                res.status(201)
-                res.send("Success")
-              }
-            })
-
-          } else {
+          } else { //Component dosen't exist.
 
             var componentID = 1
-            //Get the id of the to be created component
+            //Get the id of the to be created component.
             var queryGetID = "SELECT MAX(id) AS 'id' FROM components"
             req.db.get(queryGetID, [], (error, rowID) => {
               if (error) {
-                //If there's an error then provide the error message and the different attributes that could have caused it.
+                //If there's an error then provide the error message and the different attributes that could have caused it. 
                 res.send("ERROR! error message:" + error.message + " Input: " + inputString + ", query: " + queryGetID + ", values: " + values + ", valuesText: " + valuesText)
               } else
                 componentID += rowID.id
-              var queryInsert = "INSERT INTO components ( "
+              var queryInsert = "INSERT OR IGNORE INTO components ( "
               //Construct remaining SQL query based on input parametersInsert
               for (var j = 0; j < 2; j++) {
                 for (var i = 0; i < valuesText.length; i++) {
@@ -248,28 +335,60 @@ router.route('/:id')
                   res.send("ERROR! error message:" + error.message + " Input: " + inputString + ", query: " + queryInsert + ", values: " + values + ", valuesText: " + valuesText)
                 }
               })
-
-              //Log the creation of the components
-              var parametersLog = [componentID, new Date().toDateString(), "Component created."]
-              var queryLog = "INSERT INTO componentLog (componentID, dateLogged, note) VALUES (?, ?, ?);"
-              //console.log("currentRow: " + currentRow + ", query: " + queryInsert + "" + queryLog + ", values: " + parametersLog)
-              req.db.run((queryLog), parametersLog, (error) => {
-                if (error) {
-                  console.log(error.message)
-                  res.status(500)
-                  res.send(error.message)
-                } else {
-                  res.status(201)
-                  res.send("Success")
-                }
-              })
             })
           }
+
+          //Make sure a new componentLog isn't created if the component already existed
+          setTimeout(function () {
+            var query = "SELECT * FROM components WHERE componentName = ? AND componentVersion = ? and licenseID = ?;"
+            var parameters = [correctInputComponentName, correctInputComponentVersion, correctInputLicenseID]
+            req.db.get(query, parameters, (error, row) => {
+              if (error) {
+                console.log("1: " + error.message)
+                res.status(500)
+                res.send("ERROR! error message:" + error.message + " Input: " + inputString + ", query: " + queryInsert + ", values: " + values + ", valuesText: " + valuesText)
+              } else {
+                if (row != null) {
+                  query = "SELECT * FROM componentLog WHERE componentID = ? AND note = ?;"
+                  parameters = [row.id, "Component created."]
+                  req.db.get(query, parameters, (error, rowLog) => {
+                    if (error) {
+                      console.log(error.message)
+                      res.status(500)
+                      res.send(error.message)
+                    } else {
+                      if (rowLog == null) {
+                        //Log the creation of the component
+                        var parametersLog = [row.id, new Date().toDateString(), "Component created."]
+                        var queryLog = "INSERT INTO componentLog (componentID, dateLogged, note) VALUES (?, ?, ?);"
+                        req.db.run((queryLog), parametersLog, (error) => {
+                          if (error) {
+                            console.log(error.message)
+                            res.status(500)
+                            res.send(error.message)
+                          } else {
+                            res.status(201)
+                            res.send("Success!")
+                          }
+                        })
+                      } else {
+                        res.status(412)
+                        res.send("ERROR: Component already exists.")
+                      }
+                    }
+                  })
+                } else {
+                  res.status(412)
+                  res.send("ERROR: Provided parameters are not either not unique or not valid. Please check provided parameters and try again.")
+                }
+              }
+            })
+          }, 100)
         }
       })
 
     } else {
-      res.status(500)
+      res.status(412)
       res.send("ERROR: componentName, componentVersion, or licenseID wasn't provided.")
     }
   })
