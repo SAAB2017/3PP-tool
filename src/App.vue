a5042718dafb4beb1892de23ffca2607b382b0c2
Johannes <dat15jsu@student.lu.se>
2017-11-07 21:50:46 +0100

    Removed sidebar and centered design

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div id="app">
     <navbar></navbar>
-    <!--sidebar></sidebar-->
     <main-content></main-content>
   </div>
 </template>
 
 <script>
   import Navbar from './components/layout/Navbar'
-  import Sidebar from './components/layout/Sidebar'
   import MainContent from './components/layout/Content'
 
   export default {
     name: 'app',
     components: {
-      // Sidebar,
       Navbar,
       MainContent
     },
