7f25a2a7946161c106a436757b17c83f35bd7d7d
Javier Poremski <javier@poremski.se>
2017-11-08 17:53:46 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,25 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { polyfillLoader } from 'polyfill-io-feature-detection'
+import { polyfill } from 'es6-promise'
 import 'bulma/css/bulma.css'
 import 'font-awesome/css/font-awesome.css'
+
+polyfill()
 
 Vue.config.productionTip = false
 
 Vue.prototype.$baseAPI = 'http://localhost:3000/'
 
-/* eslint-disable no-new */
-new Vue({
+let main = new Vue({
   el: '#app',
   router,
   template: '<App/>',
   components: { App }
+})
+
+polyfillLoader({
+  "features": "Promise,fetch",
+  "onCompleted": main
 })
