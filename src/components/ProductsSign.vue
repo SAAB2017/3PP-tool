a5e18c298856fccec5a905bf18087583fe94b64f
sunnanv <dat15jsu@student.lu.se>
2017-11-27 12:23:04 +0100

    Table fix

--- src/components/ProductsSign.vue
+++ src/components/ProductsSign.vue
@@ -3,26 +3,24 @@
   <div class="products-list">
     <!-- Table that contains all unsigned products. Will grow to max-height and then
     become scrollable -->
-    <div class="vertical-menu" style="max-height: 600px; min-width: 420px">
-      <table class="table is-bordered">
-        <thead>
-        <tr>
-          <th width="45%">Product</th>
-          <th width=15%>Version</th>
-          <th width="20%">Created</th>
-          <th width="20%">Last edited</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="product in products" @click="displayComponent(product)">
-          <td>{{ product.productName}}</td>
-          <td>{{ product.productVersion }}</td>
-          <td>{{ product.dateCreated }}</td>
-          <td>{{ product.lastEdited }}</td>
-        </tr>
-        </tbody>
-      </table>
-    </div>
+    <table>
+      <thead>
+      <tr>
+        <th scope="col">Product</th>
+        <th scope="col">Version</th>
+        <th scope="col">Created</th>
+        <th scope="col">Last edited</th>
+      </tr>
+      </thead>
+      <tbody class="tbodyhome">
+      <tr v-for="product in products" @click="displayProduct(product)">
+        <td scope="row" data-label="Product">{{ product.productName }}</td>
+        <td scope="row" data-label="Version">{{ product.productVersion }}</td>
+        <td scope="row" data-label="Created">{{ product.dateCreated }}</td>
+        <td scope="row" data-label="Last edited">{{ product.lastEdited }}</td>
+      </tr>
+      </tbody>
+    </table>
 
     <!-- Field for searching for a product in the table. Uses "searchProduct"-method -->
     <div class="field has-addons columns is-mobile is-centered" style="padding-top: 15px">
@@ -47,9 +45,9 @@
         productVersion: null
       }
     },
-
     /* Fetches unsigned products from the database and puts them in products */
     mounted () {
+      const pendingURI = 'products/pending'
       axios.get(this.$baseAPI + 'products/pending')
         .then(response => {
           this.products = response.data
@@ -68,8 +66,9 @@
        * Opens the view for signing a specific product with id id.
        * @param product The product to be signed
        */
-      displayComponent (product) {
-        this.$router.push({ name: 'Sign Product', params: { id: product.id } })
+      displayProduct (product) {
+        console.log(`Product id is ${product}`)
+        this.$router.push({ name: 'products_pending_id', params: { id: product } })
       }
     }
   }
@@ -82,9 +81,5 @@
 
   tbody>tr:hover {
     cursor: pointer;
-  }
-  .vertical-menu {
-    width: 100%;
-    overflow-y: auto;
   }
 </style>
