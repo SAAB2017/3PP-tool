c361ba1c3d83e2b9d987e212a40fdc03d8349075
sunnanv <dat15jsu@student.lu.se>
2017-11-29 16:56:54 +0100

    prototype for overview

---
+++ src/components/OverviewList.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="products-list">
+
+    <div class="field has-addons columns is-mobile is-centered" style="padding-top: 15px">
+      <div class="select">
+        <select id="selected" v-on:change="selectChanged()">
+          <option value="licenses">Licenses</option>
+          <option value="components">Components</option>
+          <option value="products">Products</option>
+          <option value="projects" selected>Projects</option>
+        </select>
+      </div>
+      <div class="control">
+        <input v-on:keyup="search" v-model="searchQuery" class="input" type="text" placeholder="Search">
+      </div>
+      <div class="control">
+        <button @click="search" class="button is-primary">Search</button>
+      </div>
+    </div>
+
+    <table v-if="dispThing.length != 0">
+      <thead>
+      <tr>
+        <th scope="col">{{ selectedText }}</th>
+        <th scope="col">Version</th>
+        <th scope="col">Created</th>
+        <th scope="col">Last edited</th>
+      </tr>
+      </thead>
+      <tbody class="tbodyhome">
+      <tr v-for="comp in dispThing">
+
+        <td v-if="selected === 'products'" scope="row" data-label="Product">{{ comp.productName }}</td>
+        <td v-if="selected === 'projects'" scope="row" data-label="Product">{{ comp.projectName }}</td>
+        <td v-if="selected === 'components'" scope="row" data-label="Product">{{ comp.componentName }}</td>
+        <td v-if="selected === 'licenses'" scope="row" data-label="Product">{{ comp.licenseName }}</td>
+        <td v-if="selected === 'products'" scope="row" data-label="Version">{{ comp.productVersion }}</td>
+        <td v-if="selected === 'projects'" scope="row" data-label="Version">{{ comp.projectVersion }}</td>
+        <td v-if="selected === 'components'" scope="row" data-label="Version">{{ comp.componentVersion }}</td>
+        <td v-if="selected === 'licenses'" scope="row" data-label="Version">{{ comp.licenseVersion }}</td>
+        <td scope="row" data-label="Created">{{ comp.dateCreated }}</td>
+        <td scope="row" data-label="Last edited">{{ comp.lastEdited }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    data () {
+      return {
+        dispThing: [],
+        selected: 'projects',
+        selectedText: 'Projects',
+        searchQuery: null
+      }
+    },
+
+    methods: {
+      selectChanged () {
+        let e = document.getElementById('selected')
+        this.selected = e.options[e.selectedIndex].value
+        this.selectedText = e.options[e.selectedIndex].text
+        this.dispThing = []
+        this.searchQuery = ''
+      },
+      /**
+       * Searches for signed products from the database matching the search-criteria
+       */
+      search () {
+        if (this.searchQuery.length === 0) {
+          this.dispThing = []
+          return
+        }
+        if (this.searchQuery !== 0 || this.searchQuery !== null || this.searchQuery !== '') {
+          let query = this.selected + '/search/' + this.searchQuery
+          axios.get(this.$baseAPI + query).then(response => {
+            console.log(response.data)
+            if (response.data != null) {
+              this.dispThing = response.data
+            }
+          })
+        } else {
+          this.dispThing = []
+        }
+      },
+      /**
+       * Opens the view for a specific product with id id.
+       * @param product The product to be viewed
+       */
+      displayComponent (product) {
+        // this.$router.push({ name: 'products_id', params: { id: product.id } })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .products-list {
+    margin-bottom: 20px;
+  }
+  tbody>tr:hover {
+    cursor: pointer;
+  }
+</style>
